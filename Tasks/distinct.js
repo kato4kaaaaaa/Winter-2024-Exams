995ad13c70eccc8a6f2bbd30235d8d4901773482
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-01-14 20:33:12 +0200

    Update distinct.js
    Add strict
    Replace forEach 
    Converted Set to Array
    \

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -16,3 +16,15 @@ DISTINCT = (data) => {
 };
 
 module.exports = DISTINCT;
+
+"use strict";
+
+const distinct = (data) => {
+  const distinctSet = new Set(data);
+  const distinctArr = [...distinctSet];
+  return distinctArr;
+};
+
+
+module.exports = distinct;
+  
