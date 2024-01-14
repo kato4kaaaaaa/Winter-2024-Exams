27c0a2526f7f0b865a105f06b38967767149bc62
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-01-14 20:02:07 +0200

    Update day.js
    Use strict
    Replaced let to const
    Improved naming identifiers

--- Tasks/day.js
+++ Tasks/day.js
@@ -13,3 +13,18 @@ _parse_day_ = (s) => {
 };
 
 module.exports = _parse_day_;
+
+"use strict";
+const parseDay = (string) => {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  for (let i = 0; i < days.length; i++) {
+    const lowerCaseDay = days[i].toLowerCase();
+
+    if (!string.indexOf(lowerCaseDay)) return i +1;
+  }
+
+  return -1;
+};
+
+module.exports = parseDay;
