b6d32927469d9e42108e4c1617e527c5272d49a9
kato4kaaaaaa <155968656+kato4kaaaaaa@users.noreply.github.com>
2024-01-14 19:10:55 +0200

    Update password.js
    Removed unnecessary elements
    Renamed variables

--- Tasks/password.js
+++ Tasks/password.js
@@ -11,3 +11,14 @@ let GeneratePassword = (alphabet, length) => {
 };
 
 module.exports = GeneratePassword;
+const GeneratePassword = (alphabet, length) => {
+  const max = alphabet.length
+  let key = " "
+
+  for (let i = 0; i < length; i++) {
+    const Index = Math.floor(Math.random() * max)
+    key = key + alphabet[Index]
+ }
+
+return key 
+}  
